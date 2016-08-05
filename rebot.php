9e08f4f2499858e1deed9c18af657f477184ae22
Wscats <2587575267@qq.com>
2016-08-06 07:41:39 +0800

    add baiduApi

--- rebot.php
+++ rebot.php
@@ -5,18 +5,13 @@ $apiURL = "http://www.tuling123.com/openapi/api?key=KEY&info=INFO";
 // 设置报文头, 构建请求报文
 header("Content-type: text/html; charset=utf-8");
 $reqInfo = "讲个笑话";
-//$reqInfo = $_GET["q"];
+//请求参数
+$reqInfo = $_GET["q"]?$_GET["q"]:'你好';
+//请求的函数名
+$callback = $_GET["callback"]?$_GET["callback"]:'JSONCALLBACK';
 $url = str_replace("INFO", $reqInfo, str_replace("KEY", $apiKey, $apiURL));
 $res = file_get_contents($url);
-//echo $res;
- 
-//$q = $_GET["q"];
-//$reqInfo = $_GET["q"];
-//如果 q 大于 0，则查找数组中的所有提示
- 
-$response = json_decode($res);
-//echo $q;
-//输出响应
-var_dump($response)
-//echo $response->text;
+//JSONP模拟的请求
+//http://localhost/node/rebot.php?q=%E4%BD%A0%E5%A5%BD&callback=JSONCALLBACK
+echo $callback."(".$res.")";
 ?>
