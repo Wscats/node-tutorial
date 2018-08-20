0f218aa641f97cc1cbc7d29a5043b93b0ff169de
Eno <eno@Enode-MacBook-Pro.local>
2018-08-20 08:47:26 +0800

    改变background.js和content_script.js之间的通信
    change the connetion between bg.js and cs.js

--- WechatAutoReply/ChromeExtensions/static/js/index.js
+++ WechatAutoReply/ChromeExtensions/static/js/index.js
@@ -14,8 +14,8 @@ for (let i = 0; i < iframe.length; i++) {
 }
 // 用这个方法来ajax请求,记得要设置permissions权限
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://www.tuling123.com/openapi/api?key=c75ba576f50ddaa5fd2a87615d144ecf&info=你好", true);
-xhr.onreadystatechange = function() {
+xhr.open("GET", "https://www.tuling123.com/openapi/api?key=c75ba576f50ddaa5fd2a87615d144ecf&info=梁静茹", true);
+xhr.onreadystatechange = function () {
   if (xhr.readyState == 4) {
     console.log(xhr.responseText)
   }
@@ -29,7 +29,7 @@ meta.content = "upgrade-insecure-requests";
 document.head.appendChild(meta)
 
 // 跟隔绝区域的JS通信
-window.addEventListener("message", function(event) {
+window.addEventListener("message", function (event) {
   // 我们只接受来自我们自己的消息
   if (event.source != window)
     return;
@@ -38,3 +38,62 @@ window.addEventListener("message", function(event) {
     console.log("内容脚本接收到：" + event.data.text);
   }
 }, false);
+
+chrome.extension.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    console.log(request.action);
+    switch (request.action) {
+      case "start":
+        start();
+        break;
+      case "stop":
+        stop();
+        break;
+    }
+    console.log("前端/后端/Popup收到");
+    sendResponse({ farewell: "goodbye" });
+  }
+);
+var timer;
+var edit;
+function start() {
+  console.log("开启微信监听")
+  // 默认初始条数为空
+  var num = 0;
+  // 定时器每秒监听是否有新的会话出现
+  timer = setInterval(function () {
+    console.log("运行中")
+    // 获取微信的所有信息内容
+    var messages = $(".js_message_plain");
+    // 判断有没有新的更新条数出现，就是判断是否出现新消息
+    if (num != messages.length) {
+      // 判断发送的内容是否为空
+      if (messages[messages.length - 1].innerHTML) {
+        $.ajax({
+          type: "GET",
+          //url:"http://localhost/php/test.php?message=" + messages[messages.length - 1].innerHTML,
+          url: "http://www.tuling123.com/openapi/api?key=c75ba576f50ddaa5fd2a87615d144ecf&info=" + messages[messages.length - 1].innerHTML,
+          success(data) {
+            console.log("回复：", data);
+            edit = $('.edit_area');
+            $('.edit_area').html(data.text);
+            setTimeout(() => {
+              //$(".edit_area").trigger($.Event("keydown", { keyCode: [91, 13] }));
+              $(".edit_area").trigger($.Event("keydown", { keyCode: 13, ctrlKey: true }));
+              $('.btn_send').click();
+              console.log("点击发送消息")
+            }, 1000)
+          }
+        })
+      }
+      console.log("发送：", messages[messages.length - 1].innerHTML);
+      // 出现新消息则让数量相等
+      num = messages.length;
+    }
+  }, 1000)
+  // 动态插入JS脚本
+  // var script = document.createElement("script");
+  // script.src = "abc.js";
+  // document.body.appendChild(script)
+  // window.postMessage("message", "http://example.org");
+}
