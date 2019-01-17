ef399319b69991f5b74eb10c813f2974ddc4fa0b
wscats <2587575267@qq.com>
2019-01-17 21:50:19 +0800

    更新画板功能

--- WechatAutoReply/electron-lemon/main.js
+++ WechatAutoReply/electron-lemon/main.js
@@ -1,13 +1,22 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {
+  app,
+  BrowserWindow
+} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 600,
+  })
+  // 窗口是否可以改变尺寸. 默认值为true
+  mainWindow.setResizable(false)
+  mainWindow.setFullScreenable(false)
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
